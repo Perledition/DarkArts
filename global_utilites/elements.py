938a3579945b71970009e36ee2ed5c998f11e8b7
perlti <maxim.perl@henkel.com>
2019-08-21 20:44:17 +0200

    feat(Menus v1.0.2a)

--- global_utilites/elements.py
+++ global_utilites/elements.py
@@ -1,15 +1,28 @@
+import os
 import pygame
 
 
 class Button:
 
-    def __init__(self, color, x, y, width, height, text=''):
+    def __init__(self, color, h_color, x, y, width, height, text='', image=None, h_image=None, text_size=14, text_color=(255, 255, 255)):
         self.color = color
+        self.hover_color = h_color
+        self.hover_image = h_image
+        self.display_color = color
+        self.text_color = tuple(int(x - x/2) for x in self.color)
+        self.text_display_color = self.text_color
+        self.text_hover_color = text_color
+        self.text_size = text_size
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.text = text
+        self.image = image
+        self.display_image = image
+        self.textbox = False
+        self.textbox_text = ''
+        self.sound = pygame.mixer.music.load(os.path.join('sounds/', 'button.mp3'))
 
     def draw(self, win, outline=None):
         """
@@ -20,11 +33,103 @@ class Button:
         if outline:
             pygame.draw.rect(win, outline, (self.x-2, self.y-2, self.width+4, self.height+4), 0)
 
-        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height), 0)
+        pygame.draw.rect(win, self.display_color, (self.x, self.y, self.width, self.height), 0)
 
+        font = pygame.font.SysFont('Arial', self.text_size)
+        text = font.render(self.text, 1, self.text_display_color)
         if self.text != '':
-            font = pygame.font.SysFont('Arial', 20)
-            text = font.render(self.text, 1, (255, 255, 255))
-
             # display the text in the center of the rect
             win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
+
+        if self.display_image is not None:
+            x = self.x + (self.width/2 - self.image.get_width()/2)
+            y = self.y + (self.height/2 - self.image.get_height() - text.get_height())
+            win.blit(self.display_image, (x, y))
+
+        if self.textbox:
+            txy = self.height/2 + self.height/10 # the text box will be placed below the Image
+            pygame.draw.rect(win, (0, 0, 0), (self.x, txy, self.width, self.height/3), 0)
+            hover_text = font.render(self.textbox_text, 1, (255, 255, 255))
+            win.blit(hover_text, (self.x + (self.width/2 - hover_text.get_width()/2), txy + hover_text.get_height()*3))
+
+    def hover_text_up(self):
+        self.textbox = True
+
+    def hover(self, mode, hover_text=''):
+        if mode:
+            self.display_color = self.hover_color
+            self.text_display_color = self.text_hover_color
+            self.sound.play(0)
+            if self.hover_image is not None:
+                self.display_image = self.hover_image
+
+            # if hover_text != '':
+                # self.textbox_text = hover_text
+                #self.textbox = True
+
+        else:
+            self.display_color = self.color
+            self.display_image = self.image
+            self.text_display_color = self.text_color
+            self.textbox = False
+
+    def button_config(self):
+        return self.x, self.y, self.width, self.height
+
+
+# TODO: Create a multi line surface
+class MultiLineTextBox:
+
+    def __init_(self, text, rect):
+        self.text = text
+        self.box = rect
+
+    def text_to_portions(self):
+        pass
+
+
+class InputBox:
+
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color = (42, 43, 46)
+        self.text = text
+        self.txt_surface = pygame.font.SysFont('Arial', 20).render(text, True, self.color)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = self.color if self.active else (46, 116, 130)
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    print(self.text)
+                    self.text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = pygame.font.SysFont('Arial', 20).render(self.text, True, self.color)
+
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(200, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        # display solid ground
+        pygame.draw.rect(screen, (255, 255, 255), self.rect, 0)
+
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        # Blit the rect.
+
+        pygame.draw.rect(screen, self.color, self.rect, 2)
--- menus/main_menus.py
+++ menus/main_menus.py
@@ -8,7 +8,7 @@ import pygame
 
 # game module imports
 from settings.global_constants import window_heigth, window_width
-from global_utilites.elements import Button
+from global_utilites.elements import Button, InputBox
 from global_utilites.utilits import in_rect, define_rect
 
 
@@ -22,6 +22,7 @@ class WindowManager:
         self.height = window_heigth
         self.win = pygame.display.set_mode((self.width, self.height))
         self.font = pygame.font.Font('freesansbold.ttf', 115)
+        self.stage = 0
 
 
 class MainMenu(WindowManager):
@@ -34,20 +35,26 @@ class MainMenu(WindowManager):
         self.win.blit(self.background, (0, 0))
 
     def start_screen(self):
-        join_button = Button((46, 116, 130), 310, 536, 150, 50, 'Enter the Arena')
+        join_button = Button((46, 116, 130), (42, 43, 46), 150, 536, 150, 50, 'Enter the Arena')
+        username = InputBox(150, 500, 150, 30)
         while self.intro:
             self.clock.tick(60)
 
             # draw all elements
             self.draw_menu()
             join_button.draw(self.win)
+            username.draw(self.win)
             pygame.display.update()
 
             for event in pygame.event.get():
                 pos = pygame.mouse.get_pos()
-                rec = (join_button.x, join_button.y, join_button.x + join_button.width,
-                       join_button.y + join_button.height)
-                rec = define_rect(rec)
+                rec = define_rect(join_button.button_config())
+
+                if event.type == pygame.QUIT:
+                    done = True
+
+                for box in [username]:
+                    box.handle_event(event)
 
                 if event.type == pygame.QUIT:
                     self.intro = False
@@ -59,30 +66,111 @@ class MainMenu(WindowManager):
                         print('Start Game')
 
                 if event.type == pygame.MOUSEMOTION:
-
                     if in_rect(pos, rec):
-                        join_button.color = (42, 43, 46)
+                        join_button.hover(True)
                     else:
-                        join_button.color = (46, 116, 130)
+                        join_button.hover(False)
 
-    def text_objects(self, text):
-        text_surface = self.font.render(text, True, (21, 99, 194))
-        return text_surface, text_surface.get_rect()
-
-    def message_display(self, text):
-        text_surf, text_rect = self.text_objects(text)
-        text_rect.center = ((self.width / 2), (self.height / 2))
-        self.win.blit(text_surf, text_rect)
-        pygame.display.update()
+            for box in [username]:
+                box.update()
 
 
 class HoseSelection(WindowManager):
+
+    def __init__(self):
+        super().__init__()
+        self.selected = False
+        self.griff_img = [pygame.image.load(os.path.join('images/', x)) for x in ['griff_color.png', 'griff_gray.png']]
+        self.huff_img = [pygame.image.load(os.path.join('images/', x)) for x in ['huff_color.png', 'huff_gray.png']]
+        self.rave_img = [pygame.image.load(os.path.join('images/', x)) for x in ['rave_color.png', 'rave_gray.png']]
+        self.slys_img = [pygame.image.load(os.path.join('images/', x)) for x in ['slys_color.png', 'slys_gray.png']]
+
+        house_list = ['{}_attr.png'.format(i) for i in ['griff', 'slys', 'huff', 'rave']]
+        self.attributes = [pygame.image.load(os.path.join('images/', x)) for x in house_list]
+        self.gradient = pygame.image.load(os.path.join('images/', 'gradient.png'))
+
+    def house_screen(self):
+        w, h = pygame.display.get_surface().get_size()
+        bw = w/4
+        b_height = h - 50  # 50 pixels are needed to display the text
+
+        # define button
+        r_button = Button((56, 54, 54), (13, 96, 166), 0, 0, bw, b_height, 'Ravenclaw', self.rave_img[1], self.rave_img[0])
+        h_button = Button((56, 54, 54), (227, 190, 2), 1 * bw, 0, bw, b_height, 'Huffelpuff', self.huff_img[1], self.huff_img[0])
+        g_button = Button((56, 54, 54), (184, 22, 22), 2 * bw, 0, bw, b_height, 'Griffendor', self.griff_img[1], self.griff_img[0])
+        s_button = Button((56, 54, 54), (8, 156, 16), 3 * bw, 0, bw, b_height, 'Slytherin', self.slys_img[1], self.slys_img[0])
+        self.clock.tick(60)
+
+        while not self.selected:
+            # font = pygame.font.SysFont('Arial', 20)
+            # text = font.render('Pick your house', 1, (255, 255, 255))
+            # self.win.blit(text, (
+            # self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
+
+            r_button.draw(self.win)
+            g_button.draw(self.win)
+            h_button.draw(self.win)
+            s_button.draw(self.win)
+
+            self.win.blit(self.gradient, (0, 0))
+            pygame.display.update()
+
+            pos = pygame.mouse.get_pos()
+            r_rect = define_rect(r_button.button_config())
+            g_rect = define_rect(g_button.button_config())
+            h_rect = define_rect(h_button.button_config())
+            s_rect = define_rect(s_button.button_config())
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.selected = True
+                    pygame.quit()
+                    quit()
+
+                # check in which button the the mouse is and trigger action on click and create hover
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if in_rect(pos, g_rect):
+                        print('you picked Griffendor')
+
+                    if in_rect(pos, r_rect):
+                        print('you picked Ravenclaw')
+
+                    if in_rect(pos, h_rect):
+                        print('you picked Huffelpuff')
+
+                    if in_rect(pos, s_rect):
+                        print('you picked Slytherin')
+
+                if event.type == pygame.MOUSEMOTION:
+
+                    if in_rect(pos, g_rect):
+                        g_button.hover(True, 'Griffendor members are brave and strong:\n +5% life\n +2% damage\n -2% magic refresh rate')
+                        self.win.blit(self.attributes[0], (100, 200))
+                    else:
+                        g_button.hover(False)
+
+                    if in_rect(pos, r_rect):
+                        r_button.hover(True, 'Ravenclaw members are intelligent and creative:\n +5% magic refresh rate\n +2% stronger shild\n -2% life')
+                    else:
+                        r_button.hover(False)
+
+                    if in_rect(pos, h_rect):
+                        h_button.hover(True, 'Huffelpuff members are patience and loyal:\n +5% life\n +2% stronger shild\n -2% damage')
+                    else:
+                        h_button.hover(False)
+
+                    if in_rect(pos, s_rect):
+                        s_button.hover(True, 'Slytherin members are ambitious and cunning:\n +5% damage hit\n +2% critical hit\n -2% life')
+                    else:
+                        s_button.hover(False)
+
+
+class WandSelection(WindowManager):
     pass
 
 
-class WandSeleciton(WindowManager):
-    pass
+p = HoseSelection()
+p.house_screen()
 
-
-p = MainMenu()
-p.start_screen()
+#p = MainMenu()
+#p.start_screen()
